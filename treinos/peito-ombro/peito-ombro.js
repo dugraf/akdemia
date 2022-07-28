9c600c36a57d1c740c58e13cf5eb5172e7f6ea7e
Eduardo Rodrigues Graf <88995977+dugraf@users.noreply.github.com>
2022-07-27 23:21:10 -0300

    ombro e imc

--- treinos/peito-ombro/peito-ombro.js
+++ treinos/peito-ombro/peito-ombro.js
@@ -2,7 +2,6 @@ var res1 = document.getElementById('res1')
 var res2 = document.getElementById('res2')
 var res3 = document.getElementById('res3')
 var res4 = document.getElementById('res4')
-var res9 = document.getElementById('res9')
 
     var ex1 = 'supino declinado'
     var ex2 = 'crossover parte inferior'
@@ -10,24 +9,29 @@ var res9 = document.getElementById('res9')
     var ex3 = 'supino reto'
     var ex4 = 'voador'
 //
-    var ex7 = 'ombro no banco'
-    var ex8 = 'polia alta com puxador'
-    var ex9 = 'polia alta com corda'
+    var ex5 = 'desenvolvimento'
+    var ex6 = 'elevação frontal no crossover'
+    var ex7 = 'elevação lateral na máquina'
+    var ex8 = 'elevação lateral no banco'
+    var ex9 = 'elevação lateral no crossover'
 
     var exercicios_peito_medial = []
     exercicios_peito_medial.push(ex3, ex4)
     var array_peito_medial
 
-    var exercicios_ombro = []
-    exercicios_ombro.push(ex7, ex8, ex9)
-    var array_ombro
+    var exercicios_ombro1 = []
+    exercicios_ombro1.push(ex5, ex6)
+    var array_ombro1
 
-    var a = 12
+    var exercicios_ombro2 = []
+    exercicios_ombro2.push(ex7, ex8, ex9)
+    var array_ombro2
 
 function gerar()
 {
     array_peito_medial = arrayAleatoria(exercicios_peito_medial)
-    array_ombro = arrayAleatoria(exercicios_ombro)
+    array_ombro1 = arrayAleatoria(exercicios_ombro1)
+    array_ombro2 = arrayAleatoria(exercicios_ombro2)
     exerciciosPeito()
     exerciciosOmbro()
     
@@ -65,27 +69,70 @@ function fotosPeito()
 
 function exerciciosOmbro()
 {
-    res5.innerHTML = `${array_ombro[0]}`
-    res6.innerHTML = `${array_ombro[1]}`
-    res7.innerHTML = `${array_ombro[2]}`
-    res8.innerHTML = `polia alta com corda DROPSET`
+    res5.innerHTML = `${array_ombro1[0]}`
+    res6.innerHTML = `${array_ombro2[0]}`
+    res7.innerHTML = `${array_ombro2[1]}`
+    res8.innerHTML = `${array_ombro2[2]}`
 
     fotosOmbro()
 }
 
 function fotosOmbro()
 {
-    if (array_ombro[0] == 'ombro no banco')
+    if (array_ombro1[0] == 'desenvolvimento')
     {
-        document.getElementById("img5").innerHTML = "<img src = '../../imagens/ombro/ombro-testa-deitado-no-banco-com-barra.gif'>"
+        document.getElementById("img5").innerHTML = "<img src = '../../imagens/ombro/ombros-desenvolvimento-de-ombros-frontal-com-barra.gif'>"
     }
 
-    else if (array_ombro[0] == 'polia alta com puxador')
+    else if (array_ombro1[0] == 'elevação frontal no crossover')
     {
-        document.getElementById("img5").innerHTML = "<img src = '../../imagens/ombro/puxador.png'>"
+        document.getElementById("img5").innerHTML = "<img src = '../../imagens/ombro/elevacao-frontal-crossover.png'>"
     }
 
-    document.getElementById("img8").innerHTML = "<img src = '../../imagens/triceps/triceps-puxada-no-pulley-com-corda.gif'>"
+    if (array_ombro2[0] == 'elevação lateral na máquina')
+    {
+        document.getElementById("img6").innerHTML = "<img src = '../../imagens/ombro/elevacao-alteral-maquina.png'>"
+    }
+
+    else if (array_ombro2[0] == 'elevação lateral no banco')
+    {
+        document.getElementById("img6").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-banco.png'>"
+    }
+
+    else if (array_ombro2[0] == 'elevação lateral no crossover')
+    {
+        document.getElementById("img6").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-crossover.png'>"
+    }
+
+    if (array_ombro2[1] == 'elevação lateral na máquina')
+    {
+        document.getElementById("img7").innerHTML = "<img src = '../../imagens/ombro/elevacao-alteral-maquina.png'>"
+    }
+
+    else if (array_ombro2[1] == 'elevação lateral no banco')
+    {
+        document.getElementById("img7").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-banco.png'>"
+    }
+
+    else if (array_ombro2[1] == 'elevação lateral no crossover')
+    {
+        document.getElementById("img7").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-crossover.png'>"
+    }
+
+    if (array_ombro2[2] == 'elevação lateral na máquina')
+    {
+        document.getElementById("img8").innerHTML = "<img src = '../../imagens/ombro/elevacao-alteral-maquina.png'>"
+    }
+
+    else if (array_ombro2[2] == 'elevação lateral no banco')
+    {
+        document.getElementById("img8").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-banco.png'>"
+    }
+
+    else if (array_ombro2[2] == 'elevação lateral no crossover')
+    {
+        document.getElementById("img8").innerHTML = "<img src = '../../imagens/ombro/elevacao-lateral-crossover.png'>"
+    }
 }
 
 function arrayAleatoria(n)
